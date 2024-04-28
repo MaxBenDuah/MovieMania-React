9d94334e1676c60609c180abc502b31460896751
MaxBenDuah <maxwellben728@gmail.com>
2024-04-28 12:56:32 +0100

    Add borderbottom to search list

--- src/components/WatchedMovies/WatchedMovies.jsx
+++ src/components/WatchedMovies/WatchedMovies.jsx
@@ -1,17 +1,21 @@
 import Watched from "./Watched";
+import styles from "./WatchedMovies.module.scss";
 
 function WatchedMovies({ watchedMoviesData, onDeleteWatchedMovie, rating }) {
   return (
-    <ul>
-      {watchedMoviesData.map((movie) => (
-        <Watched
-          key={movie.id}
-          movie={movie}
-          onDeleteWatchedMovie={onDeleteWatchedMovie}
-          rating={rating}
-        />
-      ))}
-    </ul>
+    <div className={styles.watchedMoviesMainCont}>
+      <h2>Watched Movies</h2>
+      <ul>
+        {watchedMoviesData.map((movie) => (
+          <Watched
+            key={movie.id}
+            movie={movie}
+            onDeleteWatchedMovie={onDeleteWatchedMovie}
+            rating={rating}
+          />
+        ))}
+      </ul>
+    </div>
   );
 }
 
