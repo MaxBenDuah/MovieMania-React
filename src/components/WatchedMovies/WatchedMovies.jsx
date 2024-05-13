d5aca300f467394ebffa08ddc8e10b3ece0b2525
MaxBenDuah <maxwellben728@gmail.com>
2024-05-13 16:38:24 +0100

    Add default message if watched movies is empty

--- src/components/WatchedMovies/WatchedMovies.jsx
+++ src/components/WatchedMovies/WatchedMovies.jsx
@@ -1,7 +1,21 @@
+import Message from "../Message/Message";
 import Watched from "./Watched";
 import styles from "./WatchedMovies.module.scss";
 
 function WatchedMovies({ watchedMoviesData, onDeleteWatchedMovie, rating }) {
+  //Displaying a message in the watchedMovies box if there are no watched movies added
+
+  if (!watchedMoviesData.length)
+    return (
+      <div className={styles.message}>
+        <h2>Watched Movies</h2>
+        <Message
+          className={styles.messageDets}
+          messsage="👋 Hey fellow movie enthusiast, step into the cinematic universe! Add your favorite film to your watched list by searching and rating it here! 🎥🍿"
+        />
+      </div>
+    );
+
   return (
     <div className={styles.watchedMoviesMainCont}>
       <h2>Watched Movies</h2>
